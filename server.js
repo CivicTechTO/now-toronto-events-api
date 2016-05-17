005b5d758f5a685c5b97814eacd2671d91195c34
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-05-17 00:21:25 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,26 @@
+var argo = require('argo');
+var fs = require('fs');
+
+argo()
+  .use(function(handle) {
+    handle('response', function(env, next) {
+      env.response.setHeader('Access-Control-Allow-Origin', '*');
+      console.log(env.target);
+      next(env);
+    });
+  })
+  .get('^/categories$', function(handle) {
+    handle('request', function(env, next) {
+      categories = JSON.parse(fs.readFileSync('categories.json', 'utf8')).categories;
+      env.response.statusCode = 200;
+      env.response.body = JSON.stringify(categories, null, 2);
+      next(env);
+    })
+  })
+  .get('^/events$', function(handle) {
+    handle('request', function(env, next) {
+      env.target.url = 'https://nowtoronto.com/api/search/event/all/get_search_results';
+      next(env);
+    })
+  })
+  .listen(process.env.PORT || 1337);
