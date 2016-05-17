ed9a3b8d693478faa383b4311f6a5002f581a3c6
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-05-17 02:42:51 -0400

    Added venue endpoint.

--- server.js
+++ server.js
@@ -20,9 +20,36 @@ argo()
       next(env);
     })
   })
+  .get('^/venues$', function(handle) {
+    handle('request', function(env, next) {
+      url = 'https://nowtoronto.com/api/search/location/all/get_map_search_results?tile=0,0,0';
+      request(url, function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+          body = JSON.parse(body);
+          var items = []
+          body.results.forEach(function(result) {
+            var item = {
+              uid: result[0],
+              name: result[1],
+              slug: result[2],
+              url: 'https://nowtoronto.com/locations/' + result[2] + '/',
+              lat: result[3],
+              lng: result[4],
+            }
+            items.push(item);
+          })
+          body.results = items;
+          env.response.statusCode = 200;
+          env.response.body = JSON.stringify(body, null, 2);
+        }
+        next(env);
+      })
+    })
+  })
   .get('^/events$', function(handle) {
     handle('request', function(env, next) {
-      request('https://nowtoronto.com/api/search/event/all/get_search_results', function(err, res, body) {
+      url = 'https://nowtoronto.com/api/search/event/all/get_search_results';
+      request(url, function(err, res, body) {
         if (!err && res.statusCode == 200) {
           body = JSON.parse(body);
           body.per_page = body.rpp;
@@ -48,7 +75,6 @@ argo()
         }
         next(env);
       })
-      //env.target.url = 'https://nowtoronto.com/api/search/event/all/get_search_results';
     })
   })
   .listen(process.env.PORT || 1337);
